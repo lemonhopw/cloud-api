64c13fb637fd71dc8997640dc29e79f3345a719b
Lemon <lemonhopw1@gmail.com>
2025-10-15 08:50:48 +0100

    Add time, version and frontend routes

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const pkg = require("./package.json");
+
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.send("Hello from the Cloud");
+});
+
+app.get("/time", (req, res) => {
+  const now = new Date();
+  res.json({
+    time: now.toLocaleString(),
+    timezone: Intl.DateTimeFormat().resolvedOptions().timeZone
+  });
+});
+
+app.get("/version", (req, res) => {
+  res.json({
+    name: pkg.name,
+    version: pkg.version,
+    description: pkg.description
+  });
 });
 
 app.listen(PORT, () => {
